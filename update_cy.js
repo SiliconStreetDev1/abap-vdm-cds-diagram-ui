b34416fd18c29bf649624a77a198e1908bd9d694
Rikardt Louw <rikardt@greenerp.co.nz>
2026-06-06 20:25:50 +1200

    Removal of fix programs

--- update_cy.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let file = 'c:/projects/Fiori/abap-vdm-cds-diagram-ui/webapp/renderer/engines/CytoscapeEngine.ts';
-let text = fs.readFileSync(file, 'utf8');
-text = text.replace('public static getCanvasState', 'public static moveNode(sViewId: string, nodeId: string, position: {x: number, y: number}): void { const ctx = this._cyContexts.get(sViewId); if (ctx && ctx.cy) { ctx.cy.$("#" + nodeId.replace(/\\./g, "\\\\.")).position(position); } }\n\n    /**\n     * @public\n     * @static\n     * @description Extracts the live X/Y canvas coordinates for layout persistence.\n     */\n    public static getCanvasState');
-fs.writeFileSync(file, text, 'utf8');
